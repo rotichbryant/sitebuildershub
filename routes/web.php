d663cffe0b93ab9962145b6449cdde7d7d4a390e
Bryant Rotich <bryantkrotich@gmail.com>
2025-02-21 11:22:46 +0300

    Update: routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
-use App\Http\Controllers\AlertsController;
-use App\Http\Controllers\AppointmentsController;
-use App\Http\Controllers\CalendarController;
-use App\Http\Controllers\ClientsController;
-use App\Http\Controllers\CompanyController;
-use App\Http\Controllers\MailController;
-use App\Http\Controllers\OverviewController;
+use App\Http\Controllers\Dashboard\ClientsController;
+use App\Http\Controllers\Dashboard\CompanyController;
+use App\Http\Controllers\Dashboard\NotificationController;
+use App\Http\Controllers\Dashboard\OverviewController;
+use App\Http\Controllers\Dashboard\PostingController;
+use App\Http\Controllers\Dashboard\ProfileController as DashboardProfileController;
+use App\Http\Controllers\Dashboard\StaffController;
+use App\Http\Controllers\Dashboard\SubscriptionController;
+use App\Http\Controllers\Dashboard\SystemController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ServicesController;
-use App\Http\Controllers\StaffController;
-use App\Http\Controllers\SystemController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -28,26 +27,19 @@ use Inertia\Inertia;
 //     return Inertia::render('Overview');
 // })->middleware(['auth', 'verified'])->name('overview');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->name('dashboard.')->prefix('dashboard')->group(function () {
     Route::get('/',             [OverviewController::class, 'index'])->name('overview');
-    Route::get('/alerts',       [AlertsController::class, 'index'])->name('alerts');
-    Route::get('/appointments', [AppointmentsController::class, 'index'])->name('appointments');
+    Route::get('/notifications',[NotificationController::class, 'index'])->name('notifications');
     Route::get('/company',      [CompanyController::class, 'index'])->name('company');
-    Route::get('/calendar',     [CalendarController::class, 'index'])->name('calendar');
     Route::get('/clients',      [ClientsController::class, 'index'])->name('clients');
-
-    // Mail Controller Routes
-    Route::get('/mail',          [MailController::class, 'index'])->name('mail');
-    Route::post('/mail',         [MailController::class, 'store'])->name('mail.store');
-    Route::post('/mail/{account}',[MailController::class, 'show'])->name('mail.show');
-    // Mail Controller Routes
-
-    Route::get('/staff',        [StaffController::class, 'index'])->name('staff');
-    Route::get('/system',       [SystemController::class, 'index'])->name('system');
-    Route::get('/services',     [ServicesController::class, 'index'])->name('services');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/subscriptions',[SubscriptionController::class, 'index'])->name('subscriptions');
+    Route::get('/postings',     [PostingController::class, 'index'])->name('postings');
+    Route::get('/staff',        [StaffController::class,    'index'])->name('staff');
+    Route::get('/system',       [SystemController::class,   'index'])->name('system');
+    Route::get('/profile',      [DashboardProfileController::class,   'index'])->name('profile');
+    // Route::get('/profile',      [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile',    [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile',   [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';

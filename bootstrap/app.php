aec0ea8e38b9f94f17a878ee58a4428cd08357b1
Bryant Rotich <bryantkrotich@gmail.com>
2025-03-10 12:44:08 +0300

    Debug: request with

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,13 +29,13 @@ return Application::configure(basePath: dirname(__DIR__))
 
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->respond(function (Response $response) {
-            if ($response->getStatusCode() === 419) {
-                return back()->with([
-                    'message' => 'The page expired, please try again.',
-                ]);
-            }
+        // $exceptions->respond(function (Response $response) {
+        //     if ($response->getStatusCode() === 419) {
+        //         return back()->with([
+        //             'message' => 'The page expired, please try again.',
+        //         ]);
+        //     }
     
-            return $response;
-        });
+        //     return $response;
+        // });
     })->create();

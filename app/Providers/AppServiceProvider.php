631e2563a205618a089b3c1d296858669d538e7a
Bryant Rotich <bryantkrotich@gmail.com>
2025-07-23 12:53:16 +0300

    Update: providers and register pesapal service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            \App\Services\PesaPalService::class
+        );
     }
 
     /**

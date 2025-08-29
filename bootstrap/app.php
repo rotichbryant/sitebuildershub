cd0f1c97b5d4c387b8d3637222506bdbbb45c543
Bryant Rotich <bryantkrotich@gmail.com>
2025-08-29 16:20:54 +0300

    Update: caching

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,8 +26,8 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+            \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
 
     })

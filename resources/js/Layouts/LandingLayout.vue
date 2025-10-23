1ca18b98ca3ff5902a67953d959a0dfd2e98d81e
Bryant Rotich <bryantkrotich@gmail.com>
2025-10-23 14:03:15 +0300

    Update: layout show placements

--- resources/js/Layouts/LandingLayout.vue
+++ resources/js/Layouts/LandingLayout.vue
@@ -15,20 +15,23 @@
         :modals="$data.modals"
         @update:modals="$data.modals = $event"
         v-if="isEmpty(auth_user)"
+        :placements="login_placements"
       />
       <SignUp
         :modals="$data.modals"
         @update:modals="$data.modals = $event"
         v-if="isEmpty(auth_user)"
+        :placements="signup_placements"
       />      
     </div>
 </template>
 <script setup lang="ts">
-import { computed, reactive, watch } from 'vue';
+import { computed, reactive, watch, onMounted } from 'vue';
 import { LandingFooter, LandingHeader } from '../Components/Landing';
 import { Login, SignUp } from '../Components/Landing/Modals';
 import { usePage } from '@inertiajs/vue3';
 import { isEmpty } from 'lodash';
+import axios from 'axios';
 
 /**
  * The computed property for the authenticated user.
@@ -50,8 +53,31 @@ const $data = reactive({
   modals: {
     login: false,
     signup: false
-  }
+  },
+  placements: []
 });
+
+const login_placements = computed( () => $data.placements.filter( placement => placement.section === 'login' ) );
+const signup_placements = computed( () => $data.placements.filter( placement => placement.section === 'signup' ) );
+
+/**
+ * Fetch the header data from the server.
+ * 
+ * @return {Promise<void>} - A promise that resolves when the data has been fetched.
+ */
+const fetch = async () => {
+  try {
+    // Fetch the header data from the server.
+    const { data:{ placements } } = await axios.get(route('landing.security'));
+    // Set the placements data on the component.
+    $data.placements = placements;
+  } catch (error) {
+    // Log the error to the console.
+    console.error('Error fetching data:', error);
+  }
+}
+
+onMounted(fetch)
 
 /**
  * Watch the signup modal and toggle the login modal

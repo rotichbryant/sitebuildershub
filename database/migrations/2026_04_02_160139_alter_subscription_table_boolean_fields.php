a311b6fb3401dd6bb4c713efd91f921490099a24
Bryant Rotich <bryantkrotich@gmail.com>
2026-04-02 19:04:05 +0300

    Fix: subscription fields

---
+++ database/migrations/2026_04_02_160139_alter_subscription_table_boolean_fields.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->tinyInteger('active')->change();     
+            $table->tinyInteger('default')->change();     
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->boolean('active')->change();     
+            $table->boolean('default')->change();     
+        });
+    }
+};
